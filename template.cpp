64cc83376ab8dc9537387c59bada92e20a40a75f
kichico <suekiti3297@gmail.com>
2021-02-11 17:56:18 +0900

    add i/o function

--- template.cpp
+++ template.cpp
@@ -5,11 +5,37 @@ using ld=long double;
 #define MOD 1e9+7
 #define ALL(x) x.begin(),x.end()
 
+vector<vector<ll>> input(ll N, ll width){
+    string str;
+    vector<vector<ll>> vec(N,vector<ll>(width));
+    for(ll i=0;i<N;++i){
+        cin>>str;
+        reverse(ALL(str));
+        for(ll j=0;j<width;++j){
+            vec[i][j]=str.back();
+            str.pop_back();
+        }
+    }
+    return vec;
+}
+void op(vector<ll> vec){
+    ll size=(ll)vec.size();
+    for(ll i=0;i<size-1;++i) cout<<vec[i]<<" ";
+    cout<<vec.back()<<endl;
+}
+
+void op(vector<vector<ll>> vec){
+    ll height=(ll)vec.size();
+    ll width=(ll)vec[0].size();
+    for(ll i=0;i<height;++i) {
+        for(ll j=0;j<width-1;++j) cout<<vec[i][j]<<" ";
+        cout<<vec[i].back()<<endl;
+    }
+}
 
 int solve(){
     ll N;
     cin>>N;
-    
     return 0;
 }
 
