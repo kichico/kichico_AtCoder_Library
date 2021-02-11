09371b198313fcde80373b70095abfce736100d4
kichico <72244593+kichico@users.noreply.github.com>
2021-02-11 16:29:09 +0900

    Add files via upload
    add some libraries

---
+++ Prime_factorize.cpp
@@ -0,0 +1,44 @@
+﻿#include "bits/stdc++.h"
+using namespace std;
+using ll=int64_t;
+using ld=long double;
+#define MOD 1e9+7
+#define ALL(x) x.begin(),x.end()
+
+vector<pair<ll,ll>> prime_factorize(ll Num){
+    vector<pair<ll,ll>> pr; //pair<primenumber(素数),Exponentiation(べき数)>
+    vector<bool> listprime(Num);
+    for(ll i=0;i<Num;++i) listprime[i]=true;
+    ll root=sqrt(Num);
+    ll res=Num;
+    for(ll i=2;i<=root;++i){
+        ll expnum=0;
+        if(listprime[i]) {
+            while(res%i==0) {
+                res/=i;
+                expnum++;
+            }
+            for(ll j=i*2;j<=root;j+=i) listprime[j]=false;
+        }
+        if(expnum!=0) pr.emplace_back(make_pair(i,expnum));
+    }
+    if(res!=1) pr.emplace_back(make_pair(res,1));
+    return pr;
+}
+
+int solve(){
+    ll N;
+    cin>>N;
+    vector<pair<ll,ll>> pr{};
+    pr=prime_factorize(N);
+    for(ll i=0;i<pr.size();++i) cout<<pr[i].first<<"^"<<pr[i].second<<endl;
+    return 0;
+}
+
+
+int main(void){
+    std::cin.tie(nullptr);
+	std::ios_base::sync_with_stdio(false);
+	std::cout << std::fixed << std::setprecision(15);
+	solve();
+}
---
+++ template.cpp
@@ -0,0 +1,22 @@
+﻿#include "bits/stdc++.h"
+using namespace std;
+using ll=int64_t;
+using ld=long double;
+#define MOD 1e9+7
+#define ALL(x) x.begin(),x.end()
+
+
+int solve(){
+    ll N;
+    cin>>N;
+    
+    return 0;
+}
+
+
+int main(void){
+    std::cin.tie(nullptr);
+	std::ios_base::sync_with_stdio(false);
+	std::cout << std::fixed << std::setprecision(15);
+	solve();
+}
