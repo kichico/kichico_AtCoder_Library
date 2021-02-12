4d3b45f45bdd0ab4f54818aa2bcb45cdaab85e85
kichico <suekiti3297@gmail.com>
2021-02-12 11:31:35 +0900

    add dRound func

---
+++ dRound.cpp
@@ -0,0 +1,13 @@
+﻿#include "bits/stdc++.h"
+using namespace std;
+using ll=int64_t;
+using ld=long double;
+#define MOD 1e9+7
+#define ALL(x) x.begin(),x.end()
+
+double dRound(double x,ll n){
+    for(ll i=0;i<n;++i) x*=10;
+    x=round(x);
+    for(ll i=0;i<n;++i) x/=10;
+    return x;
+}
